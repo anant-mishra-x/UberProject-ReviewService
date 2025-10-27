830d1815780a5d901c4ce9424a19ecb4345919d4
Anant Mishra <anantmishra@Anants-MacBook-Air.local>
2025-10-27 13:35:35 +0530

    Added Review Repository using JPA Repositry

--- src/main/java/com/example/UberReviewService/models/Review.java
+++ src/main/java/com/example/UberReviewService/models/Review.java
@@ -2,12 +2,21 @@ package com.example.UberReviewService.models;
 
 
 import jakarta.persistence.*;
+import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.util.Date;
 
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 @Table(name = "bookingreview")
 public class Review {
 
@@ -22,10 +31,15 @@ public class Review {
 
     @Column(nullable = false)
     @CreatedDate
-    Date createadAt;
+    Date createdAt;
 
     @Column(nullable = false)
     @LastModifiedDate
     Date updatedAt;
+
+    @Override
+    public String toString(){
+        return "Review :" + this.content + " " + this.rating + " " + this.createdAt;
+    }
 
 }
